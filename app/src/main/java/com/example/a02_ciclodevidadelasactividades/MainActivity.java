49687ea8c9fc24f28e3f886f0706c199b5025574
frankribera <frank.ribera.rodriguez@cieep.com>
2022-09-20 11:57:46 +0200

    Funciones Creadas

--- app/src/main/java/com/example/a02_ciclodevidadelasactividades/MainActivity.java
+++ app/src/main/java/com/example/a02_ciclodevidadelasactividades/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.a02_ciclodevidadelasactividades;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.logging.LogManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onStart");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onResume");
     }
 }
