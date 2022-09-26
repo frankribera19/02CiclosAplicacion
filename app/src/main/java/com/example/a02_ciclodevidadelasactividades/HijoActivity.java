49ab77ca8dc0e5e8844f24f168d1ac03692e62ba
frankribera <frank.ribera.rodriguez@cieep.com>
2022-09-26 09:58:00 +0200

    Aplicación terminada

---
+++ app/src/main/java/com/example/a02_ciclodevidadelasactividades/HijoActivity.java
@@ -0,0 +1,64 @@
+package com.example.a02_ciclodevidadelasactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class HijoActivity extends AppCompatActivity {
+
+    private Button btnCerrar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hijo);
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onCreate ACTIVIDAD 2");
+
+        btnCerrar = findViewById(R.id.btnCerrarHijo);
+        btnCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onStart ACTIVIDAD 2");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onRestart ACTIVIDAD 2");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onStop ACTIVIDAD 2");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onDestroy ACTIVIDAD 2");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onPause ACTIVIDAD 2");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onResume ACTIVIDAD 2");
+    }
+}
--- app/src/main/java/com/example/a02_ciclodevidadelasactividades/MainActivity.java
+++ app/src/main/java/com/example/a02_ciclodevidadelasactividades/MainActivity.java
@@ -3,52 +3,77 @@ package com.example.a02_ciclodevidadelasactividades;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.logging.LogManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Varianbles de vista
+    private Button btnAbrirHijo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("CCICLO DE VIDA","Estoy ejecutando actividad 1");
+
+        btnAbrirHijo = findViewById(R.id.btnAbridAc2Main);
+
+        btnAbrirHijo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                 * Intent ==> Conductor para creacion y apertura de actividades
+                 *
+                 * - Implicitos: Ya programados - Camara, Galeria, Operaciones estandar del móvil
+                 * - Explicitos: Programas para brir nuestras aoctividades
+                 *      -Context -> La actividad en la que estoy (Retorno)
+                 *      -Destino -> la actividad que quiero abrir
+                 */
+                Intent intent = new Intent(MainActivity.this, HijoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        Log.e("CCICLO DE VIDA","Estoy ejecutando onStart");
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onStart ACTIVIDAD 1");
     }
 
     @Override
     protected void onRestart(){
         super.onRestart();
-        Log.e("CCICLO DE VIDA","Estoy ejecutando onRestart");
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onRestart ACTIVIDAD 1");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e("CCICLO DE VIDA","Estoy ejecutando onStop");
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onStop ACTIVIDAD 1");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e("CCICLO DE VIDA","Estoy ejecutando onDestroy");
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onDestroy ACTIVIDAD 1");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e("CCICLO DE VIDA","Estoy ejecutando onPause");
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onPause ACTIVIDAD 1");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e("CCICLO DE VIDA","Estoy ejecutando onResume");
+        Log.e("CCICLO DE VIDA","Estoy ejecutando onResume ACTIVIDAD 1");
     }
 }
